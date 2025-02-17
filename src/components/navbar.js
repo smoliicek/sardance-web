4166783291790f9b45697a4b282133cc6cb72dce
Vít Smolík <vit.smolik2@gmail.com>
2025-02-17 15:45:59 +0100

    made bar less stupid on mobile, need to fix buttons

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="w-full max-w-6xl fixed top-4 left-1/2 transform -translate-x-1/2 flex items-center justify-between p-4 bg-white bg-opacity-90 rounded-lg shadow-lg z-50">
+    <nav className="w-full max-w-6xl fixed top-4 left-1/2 transform -translate-x-1/2 flex flex-col lg:flex-row items-center justify-between p-4 bg-white bg-opacity-90 rounded-lg shadow-lg z-50">
       <div className="flex items-center ml-4">
         <Link href="/" legacyBehavior>
           <a>
@@ -20,14 +20,14 @@ export default function Navbar() {
           </a>
         </Link>
       </div>
-      <div className="lg:hidden">
+      <div className="lg:hidden mt-4 lg:mt-0">
         <button onClick={toggleMenu} className="text-black focus:outline-none">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}></path>
           </svg>
         </button>
       </div>
-      <ul className={`lg:flex space-x-4 mr-4 ${isOpen ? 'block' : 'hidden'} lg:block`}>
+      <ul className={`lg:flex space-x-4 mr-4 ${isOpen ? 'block' : 'hidden'} lg:block mt-4 lg:mt-0`}>
         <li>
           <Link legacyBehavior href="/nabidka">
             <a className="text-black bg-gray-200 rounded-full px-4 py-2 mb-4 lg:mb-0 hover:bg-gray-300 hover:shadow-lg hover:shadow-[#cb3298]/50 transition duration-300">Co nabízíme?</a>
