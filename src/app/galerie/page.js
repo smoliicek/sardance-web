85d495718fd44f1c309944d1e2b5d2910166a52a
Vít Smolík <vit.smolik2@gmail.com>
2025-02-17 21:22:05 +0100

    start galerie

---
+++ src/app/galerie/page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from "../../components/navbar";
+import Footer from '../../components/footer';
+
+export default function Kontakt() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex-grow p-8 sm:p-20 flex flex-col items-center justify-center">
+        <div className="mt-20 text-center">
+          <h1 className="text-4xl font-bold mb-8">Galerie</h1>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
